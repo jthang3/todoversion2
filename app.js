f5b4a36b6c2722263dd46fa455df4c31186ee0e1
John Lyin <jthang3@gmail.com>
2022-02-12 16:50:17 -0500

    first commit

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const controller = require("./controllers/userController");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 db.sync();
 
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "cline/build")));
+}
+
 app.use("/user",controller);
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000");
+app.listen(PORT, () => {
+    console.log(`App is listening to ${PORT}`);
 })
