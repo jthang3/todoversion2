7595c6fcc88287b695df311ae663175605e8f900
John Lyin <jthang3@gmail.com>
2022-02-12 16:13:47 -0500

    work

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize('finalToDO', 'postgres', 'Josephthang1!', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
